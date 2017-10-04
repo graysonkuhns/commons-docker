250780a180a0afd495fd535e864f5c43d0733ccb
Grayson Kuhns <grayson.kuhns@xellitix.com>
2017-10-04 09:38:48 -0500

    SNAPSHOT

--- src/main/java/com/xellitix/commons/docker/command/AbstractDockerCommandBuilder.java
+++ src/main/java/com/xellitix/commons/docker/command/AbstractDockerCommandBuilder.java
@@ -1,32 +1,81 @@
 package com.xellitix.commons.docker.command;
 
 import com.xellitix.commons.docker.image.DockerImage;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Abstract {@link DockerCommandBuilder} implementation.
  *
  * @author Grayson Kuhns
  */
-public abstract class AbstractDockerCommandBuilder<T extends DockerCommand, X extends AbstractDockerCommandBuilder>
-    implements DockerCommandBuilder<T, X> {
+public abstract class AbstractDockerCommandBuilder<F extends Enum, C extends DockerCommand<F>, B extends AbstractDockerCommandBuilder<F, C, B>>
+    implements DockerCommandBuilder<F,C, B> {
 
   // Properties
+  private Set<F> flags;
   private DockerImage image = null;
 
   /**
-   * Sets the {@link DockerImage} of the {@link T}.
-   *
-   * @param image The {@link DockerImage}.
-   * @return The {@link X}.
+   * Constructor.
    */
-  @Override
-  public synchronized X withImage(final DockerImage image) {
-    this.image = image;
-    return (X) this;
+  public AbstractDockerCommandBuilder() {
+    flags = new HashSet<>();
+    image = null;
   }
 
   /**
-   * Gets the {@link DockerImage} set in the {@link X}.
+   * Sets a flag.
+   *
+   * @param flag The flag.
+   */
+  @Override
+  public synchronized void setFlag(final F flag) {
+    flags.add(flag);
+  }
+
+  /**
+   * Sets the flags.
+   *
+   * @param flags The flags.
+   */
+  @Override
+  public synchronized void setFlags(final Set<F> flags) {
+    this.flags = flags;
+  }
+
+  /**
+   * Gets the flags.
+   *
+   * @return The flags.
+   */
+  @Override
+  public synchronized Set<F> getFlags() {
+    return flags;
+  }
+
+  /**
+   * Clears the flags.
+   */
+  @Override
+  public synchronized void clearFlags() {
+    flags.clear();
+  }
+
+  /**
+   * Sets the {@link DockerImage}.
+   *
+   * @param image The {@link DockerImage}.
+   * @return The builder.
+   */
+  @Override
+  public synchronized B withImage(final DockerImage image) {
+    this.image = image;
+    return (B) this;
+  }
+
+  /**
+   * Gets the {@link DockerImage}.
    *
    * @return The {@link DockerImage}.
    */
--- src/main/java/com/xellitix/commons/docker/command/DockerCommandBuilder.java
+++ src/main/java/com/xellitix/commons/docker/command/DockerCommandBuilder.java
@@ -1,27 +1,54 @@
 package com.xellitix.commons.docker.command;
 
 import com.xellitix.commons.docker.image.DockerImage;
+import java.util.Set;
 
 /**
  * {@link DockerCommand} builder.
  *
- * @param <T> The {@link DockerCommand} type.
- * @param <X> The {@link DockerCommandBuilder} type.
+ * @param <C> The {@link DockerCommand} type.
+ * @param <B> The {@link DockerCommandBuilder} type.
  *
  * @author Grayson Kuhns
  */
-public interface DockerCommandBuilder<T extends DockerCommand, X extends DockerCommandBuilder> {
+public interface DockerCommandBuilder<F extends Enum, C extends DockerCommand<F>, B extends DockerCommandBuilder<F, C, B>> {
 
   /**
-   * Sets the {@link DockerImage} of the {@link DockerCommand}.
+   * Sets a flag.
+   *
+   * @param flag The flag.
+   */
+  void setFlag(F flag);
+
+  /**
+   * Sets the flags.
+   *
+   * @param flags The flags.
+   */
+  void setFlags(Set<F> flags);
+
+  /**
+   * Gets the flags.
+   *
+   * @return The flags.
+   */
+  Set<F> getFlags();
+
+  /**
+   * Clears the flags.
+   */
+  void clearFlags();
+
+  /**
+   * Sets the {@link DockerImage}.
    *
    * @param image The {@link DockerImage}.
    * @return The {@link DockerCommandBuilder}.
    */
-  X withImage(DockerImage image);
+  B withImage(DockerImage image);
 
   /**
-   * Gets the {@link DockerImage} set in the {@link DockerCommandBuilder}.
+   * Gets the {@link DockerImage}.
    *
    * @return The {@link DockerImage}.
    */
@@ -32,5 +59,5 @@ public interface DockerCommandBuilder<T extends DockerCommand, X extends DockerC
    *
    * @return The {@link DockerCommand}.
    */
-  T build();
+  C build();
 }
